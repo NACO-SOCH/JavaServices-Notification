59a4544bbd4cff728cb41a04db0cb68466d06e41
Vimod Chandra <vimod.chandra@ust-global.com>
2020-10-13 16:38:33 +0530

    unused import removed.

--- src/main/java/gov/naco/soch/notification/sender/EmailSenderService.java
+++ src/main/java/gov/naco/soch/notification/sender/EmailSenderService.java
@@ -1,14 +1,11 @@
 package gov.naco.soch.notification.sender;
 
-import java.io.File;
-
 import javax.mail.internet.MimeMessage;
 
 import org.apache.commons.codec.CharEncoding;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
