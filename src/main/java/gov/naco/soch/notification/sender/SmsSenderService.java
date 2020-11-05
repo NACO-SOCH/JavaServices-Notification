02879a8d57e4830f77ea56e5e3eaeb181464b0bc
Rishad Kavattubasheer(UST <U76718@ust-global.com>
2020-11-06 03:12:05 +0530

    Changes in SMS Service!

--- src/main/java/gov/naco/soch/notification/sender/SmsSenderService.java
+++ src/main/java/gov/naco/soch/notification/sender/SmsSenderService.java
@@ -1,5 +1,18 @@
 package gov.naco.soch.notification.sender;
 
+import javax.net.ssl.SSLContext;
+
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.BasicHttpClientConnectionManager;
+import org.apache.http.ssl.SSLContexts;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +21,8 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -47,9 +62,34 @@ public class SmsSenderService {
 					.queryParam("message", smsTemplate).queryParam("mnumber", mobileNumber)
 					.queryParam("signature", smsApiSignature);
 			HttpEntity<?> entity = new HttpEntity<>(headers);
+			//---------------------
 			logger.info("Going to send SMS to mobileNumber-->{}:", mobileNumber);
 			logger.info("SMS API URL :"+builder.toUriString());
-			restTemplate.exchange(builder.toUriString(), HttpMethod.GET, entity, String.class);
+			TrustStrategy acceptingTrustStrategy = (cert, authType) -> true;
+			SSLContext sslContext = SSLContexts.custom().loadTrustMaterial(null, acceptingTrustStrategy).build();
+			SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslContext, 
+				      NoopHostnameVerifier.INSTANCE);
+			Registry<ConnectionSocketFactory> socketFactoryRegistry = 
+				      RegistryBuilder.<ConnectionSocketFactory> create()
+				      .register("https", sslsf)
+				      .register("http", new PlainConnectionSocketFactory())
+				      .build();
+			BasicHttpClientConnectionManager connectionManager = 
+				      new BasicHttpClientConnectionManager(socketFactoryRegistry);
+				    CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(sslsf)
+				      .setConnectionManager(connectionManager).build();
+				    HttpComponentsClientHttpRequestFactory requestFactory = 
+				    	      new HttpComponentsClientHttpRequestFactory(httpClient);
+				    ResponseEntity<String> response = new RestTemplate(requestFactory)
+				    	      .exchange(builder.toUriString(), HttpMethod.GET, null, String.class);
+				   
+				    logger.info("Response : "+response);
+				    logger.info("SMS API Response Code :"+response.getStatusCode().value());
+				    
+			//---------------------
+		/*	logger.info("Going to send SMS to mobileNumber-->{}:", mobileNumber);
+			logger.info("SMS API URL :"+builder.toUriString());
+			restTemplate.exchange(builder.toUriString(), HttpMethod.GET, entity, String.class);*/
 			logger.info("Sent SMS to mobileNumber-->{}:", mobileNumber);
 			// Message.creator(new PhoneNumber(mobileNumber), new PhoneNumber(sms_number),
 			// smsTemplate).create();
