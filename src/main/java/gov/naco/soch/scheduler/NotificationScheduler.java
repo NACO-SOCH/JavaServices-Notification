7ef2701c194197bc52404159c3f3b8bcfa04e64c
Vikram Kakkar <vikram.kakkar@ust-global.com>
2021-02-01 17:27:11 +0530

    enabled twilio as it is used in mobile

--- src/main/java/gov/naco/soch/scheduler/NotificationScheduler.java
+++ src/main/java/gov/naco/soch/scheduler/NotificationScheduler.java
@@ -3,22 +3,16 @@ package gov.naco.soch.scheduler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-<<<<<<< HEAD
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.task.TaskExecutor;
-=======
 import org.springframework.beans.factory.annotation.Value;
->>>>>>> dfb4e391f077cfe052e6f9b84d1de7ca4107db28
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.threeten.bp.Instant;
 
-<<<<<<< HEAD
 import gov.naco.soch.notification.service.SendPushNotificationToBenificiaryService;
-=======
 import gov.naco.soch.notification.service.NotificationService;
->>>>>>> dfb4e391f077cfe052e6f9b84d1de7ca4107db28
 import gov.naco.soch.notification.service.WebUserNotificationService;
 
 @Component
